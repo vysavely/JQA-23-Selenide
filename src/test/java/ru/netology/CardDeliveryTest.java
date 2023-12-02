fdb570969b68353ee31f647a0182b54513bbd492
vysavely <s01g@ya.ru>
2023-12-02 17:55:48 +0300

    Test edit

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -12,15 +12,15 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
-    private String generateDate(int addDays, String pattern) {
-        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    private String deliveryDate(int days, String date) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(date));
     }
 
     @Test
-    void orderacardwithdelivery() {
+    void cardOrderAndDeliveryTest() {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Нижний Новгород");
-        String currentDate = generateDate(4, "dd.MM.yyyy");
+        String currentDate = deliveryDate(5, "dd.MM.yyyy");
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id=date] input").sendKeys(currentDate);
         $("[data-test-id='name'] input").setValue("Трутнев Олег");
